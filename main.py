75c1bdcc3c4df327fec9589848c02e175879ac4e
QuinPoley <quin.poley@gmail.com>
2021-06-09 19:54:32 -0400

    Now draws pieces on the board, able to select pieces. Moving comes next

--- main.py
+++ main.py
@@ -8,15 +8,37 @@ WIN = pygame.display.set_mode([800, 800])
 pygame.display.set_caption("Quin's Chess Game!")
 myfont = pygame.font.SysFont('Comic Sans MS', 30)
 clock = pygame.time
+colorWhite = (255, 255, 255)
+colorBlack = (0, 0, 0)
+colorRed = (255, 0, 0)
 WHITE_PIECES = []
-DARK_PIECES = []
+BLACK_PIECES = []
+All_PIECES = [] # List of rects for collidepoint()
+clicked_piece = [] # selected piece list will never be greater than 1
 
 def InitializeGameOfChess():
     WHITE_PIECES.clear()
-    DARK_PIECES.clear()
+    BLACK_PIECES.clear()
     for x in range(8): # Do all the pawns
         WHITE_PIECES.append(Pawn("white", x, 2)) # X is really the letter but numbers work
-        DARK_PIECES.append(Pawn("black", x, 7))
+        BLACK_PIECES.append(Pawn("black", x, 7))
+        if (x == 0 or x == 7):
+            WHITE_PIECES.append(Rook("white", x, 1))
+            BLACK_PIECES.append(Rook("black", x, 8))
+        elif(x == 1 or x == 6):
+            WHITE_PIECES.append(Knight("white", x, 1))
+            BLACK_PIECES.append(Knight("black", x, 8))
+        elif(x == 2 or x == 5):
+            WHITE_PIECES.append(Bishop("white", x, 1))
+            BLACK_PIECES.append(Bishop("black", x, 8))
+        elif(x == 3):
+            WHITE_PIECES.append(Queen("white", x, 1))
+            BLACK_PIECES.append(Queen("black", x, 8))
+        else:
+            WHITE_PIECES.append(King("white", x, 1))
+            BLACK_PIECES.append(King("black", x, 8))
+    for x in range(16):
+        print(BLACK_PIECES[x])
     
 def drawChessBoard():
     colorGreen = (20, 197, 20)
@@ -44,20 +66,50 @@ def drawChessBoard():
         WIN.blit(columnletter,((100*x +70),0))
 
 def drawWhite():
-    print()
+    for  something in WHITE_PIECES:
+        x = (something.letter * 100) + 50
+        y = 850 - (something.number * 100)# 1 is first index
+        rectangle = pygame.Rect(x, y, 20, 20)
+        All_PIECES.append(rectangle)
+        pygame.draw.rect(WIN, colorWhite, rectangle)
 
 def drawBlack():
-    print()
+    for  something in BLACK_PIECES:
+        x = (something.letter * 100) + 50
+        y = 850 - (something.number * 100) # 1 x 100 because 1 indexed
+        rectangle = pygame.Rect(x, y, 20, 20)
+        All_PIECES.append(rectangle)
+        pygame.draw.rect(WIN, colorBlack, rectangle)
+
+def drawSelectedPiece():
+    if(len(clicked_piece) > 0):
+        pygame.draw.rect(WIN, colorRed, clicked_piece[0])
 
 def drawChessPieces():
     drawWhite()
     drawBlack()
+    drawSelectedPiece()
 
 def drawWindow():
     WIN.fill((255,255,255))
     drawChessBoard()
-    #drawChessPieces()
+    drawChessPieces()
     pygame.display.update()
+
+def clickOnPiece(pos):
+    global clicked_piece
+    for s in All_PIECES:
+        if s.collidepoint(pos):
+            clicked_piece.clear()
+            clicked_piece.append(s)
+    #clicked_piece = [s for s in All_PIECES if s.collidepoint(pos)]  List Comprehensions still make no sense
+
+def whereClick(pos):
+    x, y = pos
+    squareX = (x //100) + 1
+    squareY = (y //100) + 1
+    return (squareX, squareY)
+    
 
 def main():
     Running = True
@@ -67,8 +119,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 Running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                clickOnPiece(pos) # some time click on piece
+                if(len(clicked_piece) > 0): # There is a piece selected, trying to move it
+                    moveTo = whereClick(pos)
+                    print(moveTo)
 
-        clock.tick(60)
+        clock.tick(30)
         drawWindow()
 
         
--- pieces.py
+++ pieces.py
@@ -23,7 +23,7 @@ class Pawn(Piece):
             print()
     
     def __str__(self):
-        return self.color + "pawn @" + self.letter.__str__() +","+ self.number.__str__()
+        return self.color + " pawn @ " + chr(97+self.letter) +","+ self.number.__str__()
 
         
 
@@ -31,27 +31,90 @@ class King(Piece):
     def __init__(self, color, letter, number):
         super().__init__(color, letter, number)
 
-        
+    def returnLegalMoves():
+        if (super().color == "white"):
+            #it can only move up that column, or capture in adjacent columns.
+            print()
+        else:
+            #it can only move down
+            print()
+    
+    def __str__(self):
+        return self.color + " King @ " + chr(97+self.letter) +","+ self.number.__str__()
 
-class Queen:
-    def __init__(self, color, letter, number):
-        super().__init__(color, letter, number)
 
-        
-
-class Bishop:
-    def __init__(self, color, letter, number):
-        super().__init__(color, letter, number)
-
-        
-
-class Knight:
-    def __init__(self, color, letter, number):
-        super().__init__(color, letter, number)
 
         
 
-class Rook:
+class Queen(Piece):
     def __init__(self, color, letter, number):
         super().__init__(color, letter, number)
+
+    def returnLegalMoves():
+        if (super().color == "white"):
+            #it can only move up that column, or capture in adjacent columns.
+            print()
+        else:
+            #it can only move down
+            print()
+    
+    def __str__(self):
+        return self.color + " Queen @ " + chr(97+self.letter) +","+ self.number.__str__()
+
+
+        
+
+class Bishop(Piece):
+    def __init__(self, color, letter, number):
+        super().__init__(color, letter, number)
+
+    def returnLegalMoves():
+        if (super().color == "white"):
+            #it can only move up that column, or capture in adjacent columns.
+            print()
+        else:
+            #it can only move down
+            print()
+    
+    def __str__(self):
+        return self.color + " Bishop @ " + chr(97+self.letter) +","+ self.number.__str__()
+
+
+
+        
+
+class Knight(Piece):
+    def __init__(self, color, letter, number):
+        super().__init__(color, letter, number)
+    
+    def returnLegalMoves():
+        if (super().color == "white"):
+            #it can only move up that column, or capture in adjacent columns.
+            print()
+        else:
+            #it can only move down
+            print()
+    
+    def __str__(self):
+        return self.color + "Knight @" + chr(97+self.letter) +","+ self.number.__str__()
+
+
+
+        
+
+class Rook(Piece):
+    def __init__(self, color, letter, number):
+        super().__init__(color, letter, number)
+
+    def returnLegalMoves():
+        if (super().color == "white"):
+            #it can only move up that column, or capture in adjacent columns.
+            print()
+        else:
+            #it can only move down
+            print()
+    
+    def __str__(self):
+        return self.color + "Rook @" + chr(97+self.letter) +","+ self.number.__str__()
+
 
